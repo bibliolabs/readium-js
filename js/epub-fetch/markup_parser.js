b5b765b2026f7449cf678d20ce23e59d5f3e2154
Michael Atwood <atwoodm@bibliolabs.com>
2016-09-20 15:28:41 -0400

    Added xml version error correcting and better logging.
    * Xml 1.1 is not supported by firefox and the xml parser fails. The
    failure does not throw an error and instead returns a failure document.
    This document is not printed to the console to help find future errors
    of this kind.
    * A fix was also put in place for the xml version 1.1.

--- js/epub-fetch/markup_parser.js
+++ js/epub-fetch/markup_parser.js
@@ -22,9 +22,36 @@ define([],
                 return self.parseMarkup(xmlString, 'text/xml');
             };
 
+
+            // Due the browser differences, if an error is found print and return as normal
+            // Also, a parsererror element is valid xml so do not stop processing since it 
+            // might not be an error
+            // See: http://stackoverflow.com/questions/11563554/how-do-i-detect-xml-parsing-errors-when-using-javascripts-domparser-in-a-cross
             this.parseMarkup = function(markupString, contentType) {
                 var parser = new window.DOMParser;
-                return parser.parseFromString(markupString, contentType);
+                var dom = parser.parseFromString(self.preParse(markupString), contentType);
+
+                var errors = dom.getElementsByTagName('parsererror');
+                if (errors.length > 0) {
+                    console.log(errors[0].textContent);
+                }
+
+                return dom; 
+            };
+
+            // This code was mostly pulled from:
+            // https://github.com/readium/readium-cfi-js/pull/45/files#diff-9909b258b32fb35ba81727d9da6faaceR34
+            this.preParse = function(markupString) {
+                if (markupString && (markupString.indexOf('version="1.1"') > 0)) {
+                    console.log('Replacing XML v1.1 with v1.0 (web browser compatibility).');
+                    console.log(markupString.substr(0, 50));
+             
+                    markupString = markupString.replace(/(<\?xml[\s\S]+?)version="1.1"([\s\S]+?\?>)/, '$1version="1.0"$2');
+             
+                    console.log(markupString.substr(0, 50));
+                }
+         
+                return markupString;
             };
 
         };
